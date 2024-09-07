fcbaea4ea8b0f57e4fc700834579f20d6fae5196
krishna <krishna04.b@gmail.com>
2024-09-07 07:32:41 +0000

    my project-terra

---
+++ elb.tf
@@ -0,0 +1,27 @@
+resource "aws_elb" "bar" {
+  name               = "pythonlife-terraform-elb"
+  availability_zones = ["ap-south-1a", "ap-south-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 3
+    unhealthy_threshold = 5
+    timeout             = 5
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  cross_zone_load_balancing = true
+  idle_timeout              = 400
+  tags = {
+    Name = "pythonlife-tf-elb"
+  }
+}
+
---
+++ main.tf
@@ -0,0 +1,111 @@
+
+provider "aws" {
+region = "ap-southeast-1"
+}
+
+resource "aws_instance" "one" {
+  ami             = "ami-0e53db6fd757e38c7"
+  instance_type   = "t2.micro"
+  key_name        = "MUMBAI-TERRA"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my app created by terraform infrastructurte by pythonlife devops krishna sir server-1" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-1"
+  }
+}
+
+resource "aws_instance" "two" {
+  ami             = "ami-0e53db6fd757e38c7"
+  instance_type   = "t2.micro"
+  key_name        = "MUMBAI-TERRA"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1b"
+  user_data       = <<EOF
+#!/bin/bash
+sudo -i
+yum install httpd -y
+systemctl start httpd
+chkconfig httpd on
+echo "hai all this is my website created by terraform infrastructurte by Pythonlife Krishna sir server-2" > /var/www/html/index.html
+EOF
+  tags = {
+    Name = "web-server-2"
+  }
+}
+
+resource "aws_instance" "three" {
+  ami             = "ami-0e53db6fd757e38c7"
+  instance_type   = "t2.micro"
+  key_name        = "MUMBAI-TERRA"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "app-server-1"
+  }
+}
+
+resource "aws_instance" "four" {
+  ami             = "ami-0e53db6fd757e38c7"
+  instance_type   = "t2.micro"
+  key_name        = "MUMBAI-TERRA"
+  vpc_security_group_ids = [aws_security_group.five.id]
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "app-server-2"
+  }
+}
+
+resource "aws_security_group" "five" {
+  name = "elb-sg"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_s3_bucket" "six" {
+  bucket = "pythonlifebucket-terra"
+}
+
+resource "aws_iam_user" "seven" {
+for_each = var.user_names
+name = each.value
+}
+
+variable "user_names" {
+description = "*"
+type = set(string)
+default = ["user1", "user2", "user3", "user4"]
+}
+
+resource "aws_ebs_volume" "eight" {
+ availability_zone = "ap-south-1a"
+  size = 40
+  tags = {
+    Name = "ebs-001"
+  }
+}
