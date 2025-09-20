2e6ac0a767169f7c75c16bfa0c388caecb6978fa
Milky19 <97377003+Milky19@users.noreply.github.com>
2025-09-20 07:29:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
   ami             = "ami-00ca32bbc84273381"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "TERRRA"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
@@ -24,7 +24,7 @@ EOF
 
 resource "aws_instance" "two" {
   ami             = "ami-00ca32bbc84273381"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "TERRRA"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1b"
@@ -43,7 +43,7 @@ EOF
 
 resource "aws_instance" "three" {
   ami             = "ami-00ca32bbc84273381"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "TERRRA"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1a"
@@ -54,7 +54,7 @@ resource "aws_instance" "three" {
 
 resource "aws_instance" "four" {
   ami             = "ami-00ca32bbc84273381"
-  instance_type   = "t2.micro"
+  instance_type   = "t3.micro"
   key_name        = "TERRRA"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "us-east-1b"
